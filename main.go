d39f687a7e8549aea5c9d9bf0b67ee960b3f6ae1
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-07-18 01:09:35 +0000

    adding run()

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 // go run main.go run <cmd> <args>
 func main() {
 
+
+   switch os.Args[1] {
+   case "run":
+    run()
+   default:
+   panic("help")
+   }
+
 }
 
 func run() {
-   
+   fmt.Printf("Running %v\n", os.Args[2:])
+
    cmd       := exec.Command()
    cmd.Stdin  = os.Stdin
    cmd.Stdout = os.Stdout
